9a87076c84fac23feb97d3bb117cd002b2196d85
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-03-09 22:36:26 +0530

    commit

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ app=Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('main.html')
 
+@app.route('/showSignUp')
+def showsignUp():
+    return render_template('signup.html')
 
+@app.route('/signUp',methods=['POST'])
+def signUp():
+
+    #read posted values from the form
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    #validating the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 
 if __name__=="__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=8000)
     app.debug=True
