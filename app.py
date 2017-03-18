68196fb04ab3b6759c7801f6136beff8f1df429e
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2017-03-18 21:43:30 +0530

    updated

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import *
+from flaskext.mysql import *
+from werkzeug import *
+
+MySQL=MySQL()
 app=Flask(__name__,static_url_path='/static')
+
+#configuring MySQL
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='bhoolnamat'
+app.config['MYSQL_DATABASE_DB']='brucky'
+app.config['MYSQL_DATABASE_HOST']='localhost'
+MySQL.init_app(app)
+
 
 @app.route("/")
 def main():
@@ -11,20 +23,41 @@ def showSignUP():
     return render_template('signup.html')
 
 
-@app.route('/signUp',methods=['POST'])
+@app.route('/signUp',methods=['POST','GET'])
 def signUp():
-    #read posted values from the form
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
+    try:
+        # read posted values from the form
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
 
-    #validating the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+        # validating the received values
+        if _name and _email and _password:
+            #call mysql
+
+            conn=MySQL.connect()
+            cursor=conn.cursor()
+            _hashed_password= generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data=cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created sucessfully!'})
+            else:
+                return json.dumps({'error':str(data[0])})
+
+        else:
+            return json.dumps({'html': '<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+
+    """finally:
+        cursor.close();
+        conn.close();"""
 
 
 if __name__=="__main__":
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='127.0.0.1', port=8190)
     app.debug=True
